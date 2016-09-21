37a7e467be18f159e673b51f4296da1ac11fc8bd
funone <serge-khv@yandex.ru>
2016-09-21 00:20:48 -0600

    Passport start backend done

--- passport-init.js
+++ passport-init.js
@@ -30,23 +30,25 @@ module.exports = function (passport) {
   });
 
   passport.use('signin', new LocalStrategy({passReqToCallback: true,},function (req, username, password, done) {
+    const functionName = 'passport.signin'; 
+    log.out('info', fileName, functionName, 'function call init');
     // check in mongo if a user with username exists or not
-    User.findOne({ public: { email:  username } }, function (err, user) {
-      // In case of any error, return using the done method
-      if (err) return done(err);
-      // Username does not exist, log the error and redirect back
+    User.findOne({ public: { email:  username }}).exec()
+    .then(function(user){
       if (!user) {
-        console.log('User Not Found with username ' + username);
+        log.out('warn', fileName, functionName, 'User Not Found with username ' + username);
         return done(null, false);
       }
-      // User exists but wrong password, log the error
       if (!isValidPassword(user, password)) {
-        console.log('Invalid Password');
+        log.out('warn', fileName, functionName, 'Invalid password for ' + username);
         return done(null, false); // redirect back to login page
       }
-      // User and password both match, return user from done method
-      // which will be treated like success
       return done(null, user);
+    })
+    .catch(function(err){
+      console.log(err);
+      log.out('error', fileName, functionName, err);
+      return done(err);
     });
   }));
 
--- public/components/login/loginController.js
+++ public/components/login/loginController.js
@@ -1,3 +1,12 @@
 app.controller('loginController', function($rootScope, $scope, $http, $location){
   console.log('[loginController.js] - loginController - Scope is up');
+  $scope.user = {username:'', password:''};
+  $scope.login = function() {
+  	$http.post('/auth/signin', $scope.user).success(function(data){
+  		if (data.state == 'success') 
+  			console.log('All good');
+  		else 
+  			console.log('There is a bobo, check server logs:' + data.message);
+  	});
+  }
 });
